b8fc4d7a8d40ad6db43b2a4e0c673d12a696234c
Konstantin <ryabovkostya91@icloud.com>
2024-02-02 20:51:47 +0300

    add task 2

---
+++ src/main/java/tasks/DiffMinMax.java
@@ -0,0 +1,36 @@
+package tasks;
+
+import java.util.Scanner;
+
+public class DiffMinMax {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введите длинну массива: ");
+        int playerDig = sc.nextInt();
+        System.out.println();
+        int[] mass = new int[playerDig];
+        System.out.println("Введите числа для того,\nчтоб узнать какие из них четные: ");
+        for (int i = 0; i < mass.length; i++) {
+            mass[i] = sc.nextInt();
+        }
+        System.out.println(diffMinMax(mass));
+
+    }
+    public static int diffMinMax(int[] mass){
+        int min = mass[0];
+        int max = mass[0];
+        for (int i = 1; i < mass.length; i++) {
+            if (mass[i] <= min){
+                min = mass[i];
+            }
+        }
+        for (int i = 1; i < mass.length; i++) {
+            if (mass[i] > max){
+                max = mass[i];
+            }
+        }
+        int diff = max - min;
+        System.out.print("Разница максимального и минимального: ");
+        return diff;
+    }
+}
